74198da0ab5d9d47c17adde160217b800c66aa20
dmitru4ok <yurii.dmytruk@mif.stud.vu.lt>
2024-11-26 01:26:59 +0200

    established passwordless ssh into created vms

--- telecomms/shell/Main.sh
+++ telecomms/shell/Main.sh
@@ -1,5 +1,10 @@
 # to be run on ubuntu 22/deb 12
 
+read -sp "VM_PASSWORD: " VM_PASS
+echo
+
+# (to be sbstituted with ansible-vault)
+# -----
 echo "CREDENTIALS REQUIRED FOR creating WEBSERVER VM: "
 read -p "OpenNebula login: " WEBSERVER_VM_UNAME
 read -sp "OpenNebula password: " WEBSERVER_VM_PASS
@@ -19,6 +24,7 @@ mkdir -p /root/auth
 echo "$WEBSERVER_VM_UNAME:$WEBSERVER_VM_PASS" > /root/auth/webserver_auth
 echo "$DB_VM_UNAME:$DB_VM_PASS" > /root/auth/db_auth
 echo "$CLIENT_VM_UNAME:$CLIENT_VM_PASS" > /root/auth/client_auth
+# -------
 
 sudo apt update
 UBUNTU_CODENAME=jammy
@@ -32,3 +38,25 @@ sudo apt update && sudo apt -y upgrade && sudo apt -y install ansible
 sudo apt install python3-pip -y
 
 sudo ansible-playbook ../ansible/instantiate.yaml
+
+WEBSERVER_PRIVATE_IP=$(awk '/\[webserver\]/ {getline; print}' /etc/ansible/hosts)
+DB_PRIVATE_IP=$(awk '/\[db\]/ {getline; print}' /etc/ansible/hosts)
+CLIENT_PRIVATE_IP=$(awk '/\[client\]/ {getline; print}' /etc/ansible/hosts)
+
+
+eval "$(ssh-agent -s)" 
+ssh-keygen -t ed25519  -N "" -f ~/.ssh/id_ed25519
+ssh-add
+sshpass -p $VM_PASS ssh-copy-id -o StrictHostKeyChecking=no yudm1317@$WEBSERVER_PRIVATE_IP
+sshpass -p $VM_PASS ssh-copy-id -o StrictHostKeyChecking=no yudm1317@$DB_PRIVATE_IP
+sshpass -p $VM_PASS ssh-copy-id -o StrictHostKeyChecking=no yudm1317@$CLIENT_PRIVATE_IP
+
+
+
+# can safely execute ansible playbooks here, for example:
+# ansible-playbook webserver.yaml -K 
+
+# but have to tune users for now. Can't do root ping-pong for some reason.
+ansible webserver -m ping --extra-vars "ansible_user=$WEBSERVER_VM_UNAME"
+ansible client -m ping --extra-vars "ansible_user=$CLIENT_VM_UNAME"
+ansible db -m ping --extra-vars "ansible_user=$DB_VM_UNAME"
